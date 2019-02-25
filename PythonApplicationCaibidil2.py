11a426f46842f7dce66cd0b16b22f8f1d4302202
SputCoder <42704536+SputCoder@users.noreply.github.com>
2019-02-25 22:28:57 +0000

    Add files via upload

---
+++ PythonApplicationCaibidil2.py
@@ -0,0 +1,12 @@
+## Caibidil 2
+# print
+print('\n\n################\n\nCaibidil 2:\n\n')
+print('Print to screen')
+
+# JOIN STRINGS TOGETHER
+message1 = "It's a good" # trims right of text
+message2 = " book!"  # THIS IS ANOTHER STRING (TEXT)
+
+message = message1 + message2
+
+print(message)
