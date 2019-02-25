ed527a3c9cdcc88ffcb4c9f61359920960fd72f2
SputCoder <42704536+SputCoder@users.noreply.github.com>
2019-02-25 22:34:19 +0000

    Add files via upload
    Caibidil 3

---
+++ PythonApplicationCaibidil3.py
@@ -0,0 +1,13 @@
+## Caibidil 3
+# print
+print('\n\n################\n\nCaibidil 3:\n\n')
+print('Print to screen')
+
+## 3
+# MAKE A LIST OR ARRAY OF GOOD BANDS... :)
+
+bands = ['Thin Lizzy','Guns n Roses','AC DC','Led Zeppelin','ZZ Top']
+#bands.sort(reverse=True) # order in reverse Z to A
+
+print('\n\n1. Print list ')
+print(bands) # prints out list on screen
