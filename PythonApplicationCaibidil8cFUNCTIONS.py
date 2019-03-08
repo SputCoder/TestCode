aa7668caab199898a005da612d6596843d6b0ce4
SputCoder <42704536+SputCoder@users.noreply.github.com>
2019-03-08 12:32:51 +0000

    Add files via upload

---
+++ PythonApplicationCaibidil8cFUNCTIONS.py
@@ -0,0 +1,21 @@
+## Caibidil 8
+# print
+print('\n\n###\n\nCaibidil 8:\n\n')
+print('functions...')
+
+# CAN PASS A VALUE TO THE FUNCTION
+# HAVE A VARIABLE OR PARAMETER INSIDE THE ROUND BRACKETS
+print('The function below:\n')
+
+username = 'Thin Lizzy' # a varible declared and initialized as a string
+
+def printToScreen(username):
+    print('Function that prints username passed to the function.')
+    print('Which is...' + username) # the plus '+' just adds 2 strings
+    print('Routine in function done.\n') ## \n just new line
+
+### NOW NEED TO CALL THE FUNCTION TO SEE IT WORKING...
+printToScreen(username) # function is now called
+
+
+print('\n\nScroll to top to view all outputs!')
