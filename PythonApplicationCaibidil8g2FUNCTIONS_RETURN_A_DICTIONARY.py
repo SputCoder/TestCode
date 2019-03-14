cb2c4b713e2e37377e5d3c4702fd169faba78a4a
SputCoder <42704536+SputCoder@users.noreply.github.com>
2019-03-14 12:48:49 +0000

    Add files via upload

---
+++ PythonApplicationCaibidil8g2FUNCTIONS_RETURN_A_DICTIONARY.py
@@ -0,0 +1,17 @@
+## Caibidil 8
+# print
+print('\n\n###\n\nCaibidil 8:\n\n')
+print('functions...')
+
+# ADD A RETURN VALUE TO THE FUNCTION
+
+def categorise_bands( band_name, band_type):  
+    band_category = { 'name': band_name, 'type': band_type }
+    return(band_category) # GIVES A VALUE TO THE FUNCTION AND RETURNS IT WHEN CALLED
+
+## FUNCTION RETURNING A DICTIONARY
+output = categorise_bands('Thin Lizzy', 'Heavy Rock') # THE FUNCTION IS CALLED
+print(output) # output is in the form of a dictionary... like a list
+
+
+print('\n\nScroll to top to view all outputs, if there are many!')
