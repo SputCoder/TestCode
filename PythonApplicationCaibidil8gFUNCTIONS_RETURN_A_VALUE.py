a62ec895ef201f5b81e82d0979acf2b12f6b8217
SputCoder <42704536+SputCoder@users.noreply.github.com>
2019-03-14 12:28:15 +0000

    Update PythonApplicationCaibidil8gFUNCTIONS_RETURN_A_VALUE.py

--- PythonApplicationCaibidil8gFUNCTIONS_RETURN_A_VALUE.py
+++ PythonApplicationCaibidil8gFUNCTIONS_RETURN_A_VALUE.py
@@ -10,14 +10,14 @@ def categorise_bands( band_name, band_type):
     # print('Band name: ' + band_name)
     # print('Type of music: '+ band_type)
     # print('Routine in function done.\n') ## \n just new line
-    band_category = band_name + ' category is ' + band_type # added a space ' ' between band and type
+    band_category = band_name + ' category is ' + band_type # (**2) added a space ' ' between band and type
     return(band_category) # GIVES A VALUE TO THE FUNCTION AND RETURNS IT WHEN CALLED
     
 
 ## FUNCTION RETURNING A VALUE
 
 output = categorise_bands('Thin Lizzy', 'Heavy Rock') # THE FUNCTION IS CALLED
-# function returns the value band_category which is equal to band_name + ' category is ' + band_category
+# function returns the value band_category which is equal to band_name + ' category is ' + band_category  SEE (**2)
 # output is assigned this returned value
 print(output) 
 
