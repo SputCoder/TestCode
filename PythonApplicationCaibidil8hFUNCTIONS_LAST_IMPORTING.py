dbe8d96a17836c5c848a9c9f95e91ba4aad87d1c
SputCoder <42704536+SputCoder@users.noreply.github.com>
2019-03-15 12:11:33 +0000

    Add files via upload

---
+++ PythonApplicationCaibidil8hFUNCTIONS_LAST_IMPORTING.py
@@ -0,0 +1,49 @@
+## Caibidil 8
+# print
+print('\n\n###\n\nCaibidil 8:\n\n')
+print('functions...')
+
+#################
+######
+###### THIS PROGRAM NEEDS THE ADDITIONAL FILE function_testing_module.py 
+###### TO WORK PROPERLY 
+###### THE PROGRAM NEEDS TO IMPORT CONTENTS OF THAT FILE... :)
+
+# CAN STORE FUNCTIONS IN A SEPARATE FILE WHICH IS A MODULE
+# AND CAN GRAB IT BY USING import
+
+import function_testing_module  # imports module that has functions
+## when naming a module/file that stores functions
+## must use _ (underscore) and not - (dash) or else get an error
+
+## OK, THE FUNCTION IS IMPORTED SO IT NOW CAN BE USED
+## BUT, TO USE THE FUNCTION MUST IDENTIFY WHICH MODULE IT IS
+## WHEN CALLING THE FUNCTION [stored on a separate file]
+
+
+## FUNCTION RETURNING A DICTIONARY
+# output = categorise_bands('Thin Lizzy', 'Heavy Rock') # THE FUNCTION IS CALLED
+
+## MUST CHANGE FUNCTION TO...
+output = function_testing_module.categorise_bands('Thin Lizzy', 'Heavy Rock')
+
+# NEED TO ADD functions-testing-module. TO FUNCTION 
+print('\nIMPORTING THE ENTIRE MODULES... THAT COULD HAVE MANY FUNCTIONS')
+print(output) # output is in the form of a dictionary... like a list
+
+###  CAN IMPORT JUST ONE FUNCTION THAT YOU REQUIRE
+### RATHER THAN IMPORTING THE WHOLE MODULE THAT MIGHT HAVE MANY FUNCTIONS
+
+print('\nIMPORTING ONLY ONE FUNCTION')
+from function_testing_module import categorise_bands
+output2 = categorise_bands('Thin Lizzy', 'Heavy Rock')
+
+### NOW DON'T NEED TO SPECIFY WHICH MODULUE JUST USE FUNCTION AS BEFORE
+
+# NEED TO ADD functions-testing-module. TO FUNCTION 
+
+print(output2) # output is in the form of a dictionary... like a list
+print('It is the same outcome.')
+
+
+print('\n\nScroll to top to view all outputs, if there are many!')
---
+++ function_testing_module.py
@@ -0,0 +1,3 @@
+def categorise_bands( band_name, band_type):  
+    band_category = { 'name': band_name, 'type': band_type }
+    return(band_category) # GIVES A VALUE TO THE FUNCTION AND RETURNS IT WHEN CALLED
